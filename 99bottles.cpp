b09a9279797b588a5989440f806d3a836311e428
William Daniel <62519913+dwilly8229@users.noreply.github.com>
2020-04-08 20:13:15 +0530

    Add files via upload

---
+++ 99bottles.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+int i;
+for(i=99;i>0;i--)
+{
+cout<<i<<" bottles of beer on the wall,"<<i<<" bottles of beer.Take one down and pass it around, "<<i<<" bottles of beer on the wall.\n\n";
+}
+
+}
