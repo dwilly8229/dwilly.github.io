53fee7b57ffe99e57533971b59c1fb5fec9fe12b
William Daniel <62519913+dwilly8229@users.noreply.github.com>
2020-04-08 20:21:53 +0530

    Update 99bottles.cpp

--- 99bottles.cpp
+++ 99bottles.cpp
@@ -5,10 +5,11 @@ using namespace std;
 int main()
 {
 
-int i;
-for(i=99;i>0;i--)
+int i,a=1;
+for(i=99;i>1;i--)
 {
 cout<<i<<" bottles of beer on the wall,"<<i<<" bottles of beer.Take one down and pass it around, "<<i<<" bottles of beer on the wall.\n\n";
 }
-
+cout<<a<<" bottles of beer on the wall,"<<a<<" bottles of beer.Take one down and pass it around, no more bottles of beer on the wall.\n\n";
+cout<<"No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall";
 }
